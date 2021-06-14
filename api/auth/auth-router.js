89ac551d4dc27279fc30b8923ab1646dcea21039
ErikBurdett <erikaburdet@gmail.com>
2021-06-14 17:43:07 -0500

    added session knex and installed dependencies

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,47 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const router = require('express').Router()
+const User = require('../users/users-model')
+const bcrypt = require('bcryptjs')
+const { checkPasswordLength, checkUsernameExists, checkUsernameFree} = require('./auth-middleware')
 
+router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
+ const { username, password } = req.body
+ const hash = bcrypt.hashSync(
+   password,
+   8,
+   )
+   User.add({ username, password: hash})
+   .then(saved => {
+     res.status(201).json(saved)
+   })
+   .catch(next)
+})
+
+router.post('/login', checkUsernameExists, (req, res, next) => {
+   const {password} = req.body;
+   if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.json({message: `Welcome ${req.user.username}`, status: 200})
+   } else {
+     next({ status: 401, message: 'Invalid credentials'})
+   }
+})
+
+router.get('/logout', (req, res, next) => {
+ if (req.session.user) {
+  req.session.destroy(err => {
+    if (err) {
+      next(err)
+    } else {
+      res.status(200).json({message: 'logged out'})
+    }
+  })
+ } else {
+  res.json({ message: 'no session', status: 200})
+ }
+})
+module.exports = {router}
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
